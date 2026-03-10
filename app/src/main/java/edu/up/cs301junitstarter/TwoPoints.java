3dca308cd32f4c96701f0c6e080358400f13552f
Matthew Rueben <rueben@up.edu>
2026-03-10 14:29:02 -0700

    Add more specific comments to each method specifying the desired behavior.

--- app/src/main/java/edu/up/cs301junitstarter/TwoPoints.java
+++ app/src/main/java/edu/up/cs301junitstarter/TwoPoints.java
@@ -18,35 +18,59 @@ public class TwoPoints {
         points[1] = new Point(0,0);
     }
 
-    /** get a point */
+    /** Gets a point.
+     * - Assume this won't be called with an invalid index.
+     */
     public Point getPoint(int index) {
         return points[index];
     }
 
-    /** set a point */
+    /** Set a point.
+     * - Assume this won't be called with an invalid index.
+     * - The x and y parameters should work as positive, negative, or zero.
+     */
     public void setPoint(int index, int x, int y) {
         points[index].x = x;
         points[index].y = y;
     }
 
-    /** assign a random value to a point */
+    /** Assign a random value to a point.
+     * - Assume this won't be called with an invalid index.
+     * - The point at that index will be given new x and y coordinates.
+     * - If you run this several times, the x and y coordinates should sometimes be positive, other times negative.
+     * - The other point should stay the same.
+     */
     public void randomValue(int index) {
         int x = rand.nextInt(20) - 10;
         int y = rand.nextInt(20) - 10;
         setPoint(index, x, y);
     }
 
-    /** sets a point to the origin */
+    /** Sets a point to the origin.
+     * - Assume this won't be called with an invalid index.
+     * - The point at that index will have its x and y coordinates set to zero.
+     * - The other point should stay the same.
+     */
     public void setOrigin(int index) {
         setPoint(index, 0, 0);
     }
 
-    /** copy the values in one points to the other */
+    /** Copy the values in one point to the other.
+     * - Assume that this won't be called with an invalid index as either argument.
+     * - The x and y coordinates of the destination point should change to match the current x and y coordinates of the source point.
+     * - The source point's coordinates should not change.
+     * - It should be a deep copy -- i.e., the two points should not be pointing to the same object after this runs.
+     */
     public void copy(int srcIndex, int destIndex) {
         points[destIndex] = points[srcIndex];
     }
 
-    /** calculates the distance between the two points rounded to the nearest integer */
+    /** Calculates the distance between the two points.
+     * - Should work regardless of whether the invisible line between the two points is horizontal, vertical, or diagonal.
+     * - Should work regardless of whether the x1-x0 is positive, negative, or zero; same for y1-y0.
+     * - Should work for fractional distances as well as whole-number ones.
+     * - Should work even when the distance is zero.
+     */
     public double distance() {
         double result = 0.0;
         int xDiff = points[0].x - points[1].x;
@@ -55,9 +79,13 @@ public class TwoPoints {
         return result;
     }
 
-    /** calculates the slope of a line passing through the two points
-     *
-     * @return the slope or 0 if the points are equal */
+    /** Calculates the slope of a line passing through the two points.
+     * - The line starts at the point at index 0 and goes to the point at index 1. (E.g., if the points are (0,0) and (1,1), the slope should be *positive* 1.)
+     * - If the line is vertical or the points are identical, we can't calculate the slope because it involves dividing by zero; this should throw an ArithmeticException (i.e., crashing is the desired behavior in this situation).
+     * - If the line is horizontal, the slope should be zero.
+     * - Should work regardless of whether the x1-x0 is positive, negative, or zero; same for y1-y0.
+     * - Should work for fractional slopes as well as whole-number slopes.
+     */
     public double slope() {
         double result = 0.0;
         if (points[0] == points[1]) {
